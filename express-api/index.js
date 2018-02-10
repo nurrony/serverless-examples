bc1aa188feef877b25e844798d1de326a82d9531
Nur Rony <pro.nmrony@gmail.com>
2018-02-10 19:12:50 +0600

    adding fully functional serverless express api example

--- express-api/index.js
+++ express-api/index.js
@@ -1,12 +1,24 @@
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const express = require('express')
-const app = express()
 const AWS = require('aws-sdk');
 
+const app = express()
 
 const USERS_TABLE = process.env.USERS_TABLE;
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDb;
+if (IS_OFFLINE === 'true') {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  })
+  console.log(dynamoDb);
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient();
+};
+
 
 app.use(bodyParser.json({ strict: false }));
 
